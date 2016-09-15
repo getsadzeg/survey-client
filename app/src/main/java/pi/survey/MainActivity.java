eb418075aa99e8bcf37707b398304aa63b416fe0
getsadzeg <getsadzeguram@gmail.com>
2016-09-15 14:32:49 +0400

    add runtime permissions

--- app/src/main/java/pi/survey/MainActivity.java
+++ app/src/main/java/pi/survey/MainActivity.java
@@ -1,6 +1,10 @@
 package pi.survey;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
@@ -21,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     EditText members;
     Survey survey = null;
     SurveyServices ss;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,23 +40,67 @@ public class MainActivity extends AppCompatActivity {
         middle_name = (EditText) findViewById(R.id.editText3);
         members = (EditText) findViewById(R.id.editText4);
 
-
-
     }
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // storage-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    public SurveyServices returnService() {
+        return ServiceGenerator.createService(SurveyServices.class, this);
+    }
+
     public void submitOnClick(View v) {
+        verifyStoragePermissions(this);
         int int_members = Integer.parseInt(members.getText().toString());
         survey = new Survey(name.getText().toString(), surname.getText().toString(),
                 middle_name.getText().toString(), int_members );
-        ss = ServiceGenerator.createService(SurveyServices.class, this);
+        ss = returnService();
         ss.submit(survey.returnJSON(), new Callback<submitResponse>() {
             @Override
             public void success(submitResponse submitResponse, Response response) {
-                System.out.println("response: " + submitResponse);
+                System.out.println("inside success method. response: " + submitResponse);
             }
 
             @Override
             public void failure(RetrofitError error) {
-
+                error.printStackTrace();
+                error.getResponse().getReason();
             }
         });
     }
