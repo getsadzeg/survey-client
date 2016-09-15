294a14efe9e03fb137cd587ca9a304d487b27947
getsadzeg <getsadzeguram@gmail.com>
2016-09-15 14:31:39 +0400

    remove field 'id'

--- app/src/main/java/pi/survey/services/submitResponse.java
+++ app/src/main/java/pi/survey/services/submitResponse.java
@@ -5,5 +5,4 @@ import com.bluelinelabs.logansquare.annotation.JsonObject;
 @JsonObject(fieldDetectionPolicy = JsonObject.FieldDetectionPolicy.NONPRIVATE_FIELDS_AND_ACCESSORS)
 public class submitResponse {
     public boolean success;
-    public int id;
 }
