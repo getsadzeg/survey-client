1b7e609a356c35ddf0d44ac0a4d5be091db2e1fd
getsadzeg <getsadzeguram@gmail.com>
2017-09-08 16:01:46 +0400

    bump versions and add error dialog

--- app/src/main/java/pi/survey/MainActivity.java
+++ app/src/main/java/pi/survey/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.support.v7.app.AlertDialog;
 
 import pi.survey.model.Survey;
 import pi.survey.services.ServiceGenerator;
@@ -23,9 +24,11 @@ public class MainActivity extends AppCompatActivity {
     EditText surname;
     EditText middle_name;
     EditText members;
+    int int_members;
     Survey survey = null;
     SurveyServices ss;
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static final String TAG = "MyActivity";
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
@@ -39,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         surname = (EditText) findViewById(R.id.editText2);
         middle_name = (EditText) findViewById(R.id.editText3);
         members = (EditText) findViewById(R.id.editText4);
+
 
     }
 
@@ -87,7 +91,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void submitOnClick(View v) {
         verifyStoragePermissions(this);
-        int int_members = Integer.parseInt(members.getText().toString());
+        if(members.getText().toString().matches("[0-9]+")) {
+            int_members = Integer.parseInt(members.getText().toString());
+            if(int_members > 25) errorDialog(R.string.error_msg);
+        }
+        else {
+            int_members = 0; //temporary. will throw exception in the future
+            android.util.Log.e(TAG, "Members should be provided!");
+        }
         survey = new Survey(name.getText().toString(), surname.getText().toString(),
                 middle_name.getText().toString(), int_members );
         ss = returnService();
@@ -103,6 +114,15 @@ public class MainActivity extends AppCompatActivity {
                 error.getResponse().getReason();
             }
         });
+    }
+
+    private void errorDialog(int message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setTitle(message);
+        builder.setPositiveButton(R.string.dialog_button_ok, null);
+        builder.show();
     }
 
 }
